d2da387130f6427a4736b691018b49c945e73109
DiegoRojo99 <diego99rojo@gmail.com>
2025-07-14 22:51:14 +0200

    Imported colors and added some more

---
+++ utils/Colors.ts
@@ -0,0 +1,31 @@
+const colors = { 
+  dark: {
+    'text': '#eff4f6',
+    'background': '#04151a',
+    'primary': '#86cadf',
+    'secondary': '#16718d',
+    'accent': '#19bef0',
+  },
+  light: {
+    'text': '#1a202c',
+    'background': '#f7fafc',
+    'primary': '#2b6cb0',
+    'secondary': '#2c5282',
+    'accent': '#63b3ed',
+  },
+  transparent: {
+    'text': 'rgba(255, 255, 255, 0.7)',
+    'background': 'rgba(255, 255, 255, 0.1)',
+    'primary': 'rgba(255, 255, 255, 0.2)',
+    'secondary': 'rgba(255, 255, 255, 0.3)',
+    'accent': 'rgba(255, 255, 255, 0.4)',
+  },
+  error: '#e53e3e',
+  success: '#38a169',
+  warning: '#eada2dff',
+  info: '#3182ce',
+  white: '#ffffff',
+  black: '#000000',
+};
+
+export default colors;
