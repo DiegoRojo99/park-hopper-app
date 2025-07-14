9103df58656cc0c0356c8c75e593fc016b1a86fb
DiegoRojo99 <diego99rojo@gmail.com>
2025-07-14 22:51:23 +0200

    Updated home screen

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,13 +1,26 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Pressable } from "react-native";
+import colors from "../utils/Colors";
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../types/navigation";
 
 export default function Home() {
+  const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+  const handleGetStarted = () => {
+    navigation.navigate("Explore");
+  };
+  
   return (
     <View style={styles.container}>
       <Text style={styles.headerText}>Park Hopper</Text>
-      <Text style={styles.text}>Welcome to the Park Hopper app!</Text>
       <Text style={styles.text}>
-        Explore theme parks, check wait times, and plan your visit.
+        Discover, plan, and enjoy your next park adventure. Find the best parks, track your visits, and share your experiences!
       </Text>
+      <View style={{ marginTop: 20 }}>
+        <Pressable style={styles.button} onPress={handleGetStarted}>
+          <Text style={styles.buttonText}>Get Started</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
@@ -20,7 +33,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#f0f0f0",
   },
   headerText: {
-    fontSize: 24,
+    fontSize: 32,
     fontWeight: "bold",
     marginBottom: 20,
     color: "#333",
@@ -28,5 +41,17 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 18,
     color: "#333",
+    textAlign: "center",
+    paddingHorizontal: 20,
+  },
+  button: {
+    backgroundColor: colors.light.primary,
+    padding: 12,
+    borderRadius: 8,
+  },
+  buttonText: {
+    fontSize: 18,
+    color: "#FFFFFF",
+    textAlign: "center",
   },
 });
