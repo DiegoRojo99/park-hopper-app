1ba8ff63b36c54031e9e6651339325c743fa0897
DiegoRojo99 <diego99rojo@gmail.com>
2025-07-15 19:16:34 +0200

    Open destination card

--- components/destinations/DestinationCard.tsx
+++ components/destinations/DestinationCard.tsx
@@ -2,49 +2,70 @@ import { StyleSheet, Text, View } from "react-native";
 import { DestinationWithParks } from "../../types/db";
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import ParkRow from "../parks/ParkRow";
+import { useState } from "react";
 
 export default function DestinationCard({ destination }: { destination: DestinationWithParks }) {
+  const [open, setOpen] = useState(false);
+
   return (
     <View style={styles.card}>
-      <View style={styles.description}>
-        <View style={{ flexDirection: "row", alignItems: "center" }}>
-          <MaterialCommunityIcons name="ferris-wheel" size={32} color="black" />
-          <View style={{ paddingHorizontal: 8, marginLeft: 8, flex: 1, borderBottomWidth: 1, borderColor: "#eee" }}>
-            <Text style={styles.title}>{destination.name}</Text>
-            <Text style={styles.description}>
-              {destination.parks.length} park{destination.parks.length !== 1 ? 's' : ''}
-            </Text>
-          </View>
+      <View style={styles.topRow}>
+        <View style={styles.chevronContainer}>
+          {open ? (
+            <MaterialCommunityIcons name="chevron-up" size={32} color="black" onPress={() => setOpen(false)} />
+          ) : (
+            <MaterialCommunityIcons name="chevron-down" size={32} color="black" onPress={() => setOpen(true)} />
+          )}
+        </View>
+        <View style={{ paddingHorizontal: 8, marginLeft: 8, flex: 1 }}>
+          <Text style={styles.title}>{destination.name}</Text>
+          <Text style={styles.description}>
+            {destination.parks.length} park{destination.parks.length !== 1 ? 's' : ''}
+          </Text>
         </View>
       </View>
-      <View style={styles.parksContainer}>
-        {destination.parks.map((park) => ( <ParkRow key={park.id} park={park} /> ))}
-      </View>
+      { open && 
+        <View style={styles.parksContainer}>
+          {destination.parks.map((park) => ( <ParkRow key={park.id} park={park} /> ))}
+        </View>
+      }
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   card: {
-    padding: 16,
-    marginBottom: 16,
+    padding: 8,
+    marginBottom: 8,
     backgroundColor: "#fff",
-    borderRadius: 8,
+    borderRadius: 4,
     shadowColor: "#000",
     shadowOffset: { width: 0, height: 1 },
     shadowOpacity: 0.2,
     shadowRadius: 1.41,
     elevation: 2,
+    width: "100%",
+    flexDirection: "column",
+  },
+  topRow: {
+    flexDirection: "row",
+  },
+  chevronContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    display: "flex",
   },
   title: {
-    fontSize: 20,
+    fontSize: 16,
     fontWeight: "bold",
   },
   description: {
-    fontSize: 16,
+    fontSize: 14,
     marginBottom: 8,
   },
   parksContainer: {
-    marginTop: 8,
+    paddingTop: 8,
+    borderTopWidth: 1,
+    borderColor: "#eee"
   },
 });
