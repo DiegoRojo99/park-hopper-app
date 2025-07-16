c7758fd48ee7f349c940b48cd3eeb363e0c1349f
DiegoRojo99 <diego99rojo@gmail.com>
2025-07-16 13:10:02 +0200

    Added tab navigation

--- App.tsx
+++ App.tsx
@@ -3,20 +3,59 @@ import { NavigationContainer } from '@react-navigation/native';
 import Home from './screens/Home';
 import Explore from './screens/Explore';
 import ParkDetails from './screens/ParkDetails';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import colors from './utils/Colors';
 
 const Stack = createNativeStackNavigator();
 const commonScreenOptions = {
   headerShown: false,
 };
 
+const BottomTabs = createBottomTabNavigator();
+const TabScreenOptions = {
+  headerShown: false,
+  tabBarInactiveTintColor: colors.light.accent,
+  tabBarActiveTintColor: '#ffffff',
+  tabBarStyle: {
+    backgroundColor: colors.light.primary,
+    marginBottom: 0,
+    paddingTop: 5,
+  },
+};
+
+function ExploreStack() {
+  return (
+    <Stack.Navigator screenOptions={commonScreenOptions}>
+      <Stack.Screen name="Explore" component={Explore} />
+      <Stack.Screen name="ParkDetails" component={ParkDetails} />
+    </Stack.Navigator>
+  );
+}
+
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={commonScreenOptions}>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Explore" component={Explore} />
-        <Stack.Screen name="ParkDetails" component={ParkDetails} />
-      </Stack.Navigator>
+      <BottomTabs.Navigator screenOptions={TabScreenOptions}>
+        <BottomTabs.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="home" size={size} color={color} />
+            ),
+          }}
+        />
+        <BottomTabs.Screen 
+          name="ExploreStack" 
+          component={ExploreStack} 
+          options={{ 
+            tabBarIcon: ({ color, size }) => (
+              <Ionicons name="search" size={size} color={color} />
+            ),
+          }}
+        />
+        </BottomTabs.Navigator>
     </NavigationContainer>
   );
 }
